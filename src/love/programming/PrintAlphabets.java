40abfdefa9fdce0bb9d5cd4a8186a8775b3f9dc1
Umadevi23 <uma_gorakati@yahoo.co.in>
2019-03-05 23:05:17 +0000

    JavaEnglishAlphabets

---
+++ src/love/programming/PrintAlphabets.java
@@ -0,0 +1,21 @@
+package love.programming;
+
+public class PrintAlphabets {
+    static char a1 = 'A';
+    static char b = 'B';
+    static char c = 'C';
+    static char d = 'D';
+    static char e = 'E';
+    static char f = 'F';
+    static char g = 'G';
+    static char h = 'H';
+    static char i = 'I';
+
+    public static void main(String[] args) {
+        System.out.println("default value of char is "+ a1);
+
+        System.out.println("default value of char is "+
+                a1 + b +c + d + e + f + g + h + i  );
+    }
+
+}
